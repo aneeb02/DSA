bc5f40bb2373c4535b8df23b519efe0aaf0809dd
aneeb02 <aneeb.02@gmail.com>
2026-02-01 22:02:54 +0500

    solved longest substring

---
+++ longestSubstring.py
@@ -0,0 +1,29 @@
+'''
+Given a string s, find the length of the longest substring without duplicate characters.
+
+A substring is a contiguous sequence of characters within a string.
+'''
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        # empty string, no substring
+        if len(s) == 0:
+            return 0
+
+        left, right = 0, 0
+        count = 0
+        seen = set()
+
+        # slide a window for substring
+        while right < len(s):
+            if s[right] not in seen:
+                seen.add(s[right])
+                count = max(count, 1 + right-left)
+                right += 1
+
+            else:
+                seen.discard(s[left])
+                left += 1
+
+        return count
+    
